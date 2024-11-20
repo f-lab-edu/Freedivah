9f90946c3180b1b6bc8d8cf97ebde0743a31a0ad
jiaah <25231717+jiaah@users.noreply.github.com>
2024-11-20 19:39:00 +0900

    chore: Migrate from Yarn to pnpm with Turborepo

--- packages/web/app/(routes)/map/page.tsx
+++ packages/web/app/(routes)/map/page.tsx
@@ -1,14 +1,7 @@
-// Next.js 특수 기능도 여기서 정의
-export const metadata = {
-	title: 'Home Page',
-};
-
-export function MapPage() {
+export default function MapPage() {
 	return (
 		<main>
-			Map Page
+			<h1>Map Page</h1>
 		</main>
-	);
+	)
 }
-
-export default MapPage;
